d6648b427108de965e8ea1d3900e07efc4766e38
sahana <shayaandoodekula950@gmail.com>
2025-11-20 22:56:05 -0500

    most techbro site ever i KNOW okay sybau :wilted_rose:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
+"use client";
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 export default function Home() {
-  return (
+	const names = [
+		"Sahana",
+		"a Web Developer",
+		"a hackathon organizer",
+		"a silly person :3",
+		"someone, somewhere."
+	];
+	const [index, setIndex] = useState(0);
+	const [slide, setSlide] = useState(true);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSlide(false);
+			setTimeout(() => {
+				setIndex((prev) => (prev + 1) % names.length);
+				setSlide(true);
+			}, 500);
+		}, 2500);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hi, I'm Sahana</h1>
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-16 px-0 bg-white dark:bg-black sm:items-start">
+        <div className={"flex flex-col w-full"}>
+	        <div className={"flex flex-row w-full"}>
+		        <h1 className={"text-3xl font-black float-right"}>👋 Hi, I&apos;m{" "}
+			        <span className={`inline-block transform transition-all duration-500 ease-in-out ${
+				        slide ? "translate-y-0 opacity-100" : "translate-y-6 opacity-0"
+			        }`}>
+				        {names[index]}
+			        </span>
+						</h1>
+		        <p className={"ml-auto text-xl font-light italic"}>goon goon goon goon goon</p>
+	        </div>
+	        <hr className={"border-t-2 border-white my-4 w-full"}/>
+        </div>
       </main>
     </div>
   );
