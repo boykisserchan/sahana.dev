fb71f5c5d50f055f2f475963517236a65152b9b7
sahana <shayaandoodekula950@gmail.com>
2025-11-22 20:21:36 -0500

    uwu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 	return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-16 px-0 bg-white dark:bg-black sm:items-start">
-        <div className={"flex flex-col w-full"}>
+        <header className={"flex flex-col w-full"}>
 	        <div className={"flex flex-row w-full"}>
 		        <h1 className={"text-3xl font-black float-right"}>Hi, I&apos;m{" "}
 			        <span className={`inline-block transform transition-all duration-500 ease-in-out ${
@@ -42,7 +42,7 @@ export default function Home() {
 		        <a href={`https://github.com/boykisserchan/sahana.dev/commit/${commit.hash}`} className={"ml-auto text-md font-light italic underline self-end-safe"}>{commit.hash + " " + commit.message}</a>
 	        </div>
 	        <hr className={"border-t-2 border-white my-4 w-full"}/>
-        </div>
+        </header>
       </main>
     </div>
   );
